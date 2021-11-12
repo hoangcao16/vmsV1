4c368cb210a1d0c28380459c4a9cb0a4587fcf74
thuong thuong <thuongdtt@edsolabs.com>
2021-11-12 16:02:09 +0700

    face tr

--- src/view/ai-humans/store-setting/redux/sagas/index.js
+++
@@ -1,7 +0,0 @@
-import { all } from 'redux-saga/effects';
-import allEmailSaga from './allEmailSaga';
-
-export default function* rootSaga() {
-  console.log('saga');
-  yield all([allEmailSaga()]);
-}
