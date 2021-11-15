76fb5bc0e4a118e4e4439f858776d66e68eceb5f
Phung Tuan <mr.bichkhe@gmail.com>
2021-11-15 16:18:58 +0700

    VR-869 [Report] Khi Không nhận dữ liệu từ backend frontend crashed

--- src/actions/function/MyUltil/ResponseChecker.js
+++ src/actions/function/MyUltil/ResponseChecker.js
@@ -152,6 +152,15 @@ const REPORT_OKE = 1300;
 const REPORT_BAD_REQUEST = 1301;
 
 export const handleErrCodeReport = (data) => {
+  if(!data){
+    const errCode = {
+      type: 'error',
+      title: 'Code:',
+      description: 'data is null'
+    };
+    Notification(errCode);
+    return null;
+  }
   const { code, message, payload, deny_permission_codes } = data;
   const errCode = {
     type: 'error',
