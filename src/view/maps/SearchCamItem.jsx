43157522540d5af59ba1f14b11773d437f2d3e91
[BK]Phù Dung <thuongtt@edsolabs.com>
2021-11-18 15:59:27 +0700

    [VR577] wip/create quick search

--- src/view/maps/SearchCamItem.jsx
+++ src/view/maps/SearchCamItem.jsx
@@ -1,35 +1,41 @@
-import React, {useRef, useState} from 'react'
-import { Input } from 'antd';
-import { SearchOutlined } from '@ant-design/icons';
+import { SearchOutlined } from "@ant-design/icons";
+import React, { useRef, useState } from "react";
+import EditableTagGroup from "./EditableTagGroup";
 
 const SearchCamItem = () => {
-    const searchCamRef = useRef(null);
-    const [expandSearch, setExpandSearch] = useState(false);
-    const handleExpandSearchBar = () => {
-        searchCamRef.current && searchCamRef.current.classList.toggle('active');
-        setExpandSearch(!expandSearch)
-    }
-    return (
-        <div ref={searchCamRef} className="search-cam-item">
-        <div className="search-box">
+  const searchCamRef = useRef(null);
+  const [expandSearch, setExpandSearch] = useState(false);
+  const handleExpandSearchBar = () => {
+    searchCamRef.current && searchCamRef.current.classList.toggle("active");
+    setExpandSearch(!expandSearch);
+  };
+  return (
+    <div ref={searchCamRef} className="search-cam-item">
+      <div className="search-box">
         <div className="input-box">
-                <input type="text" placeholder="Search...."/>
-            </div>
-            <div className="search-icon" onClick={handleExpandSearchBar}>
-                <SearchOutlined />
-            </div>
+          <input type="text" placeholder="Search...." > 
+         
+          </input>
         </div>
-            {<div className="list-data-search">
-                <ul>
-                    <li>hungng</li>
-                    <li>hungng</li>
-                    <li>hungng</li>
-                    <li>hungng</li>
-                    <li>hungng</li>
-                </ul>
-            </div>}
+        <div className="search-icon" onClick={handleExpandSearchBar}>
+          <SearchOutlined />
         </div>
-    )
-}
+      </div>
+      <EditableTagGroup />
 
-export default SearchCamItem
+      {
+        <div className="list-data-search">
+          <ul>
+            <li>hungng</li>
+            <li>hungng</li>
+            <li>hungng</li>
+            <li>hungng</li>
+            <li>hungng</li>
+          </ul>
+        </div>
+      }
+    </div>
+  );
+};
+
+export default SearchCamItem;
