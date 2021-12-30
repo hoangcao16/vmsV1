293890e89414cd314551fcd8198900d727944234
hainl <hainl@edsolabs.com>
2021-12-30 18:30:55 +0700

    VR-992

--- src/actions/api/report/ReportApi.js
+++ src/actions/api/report/ReportApi.js
@@ -1,41 +1,34 @@
-import {
-    handleErrCodeReport,
-  } from "../../function/MyUltil/ResponseChecker";
-  import MyService from "../service";
+import { handleErrCodeReport } from "../../function/MyUltil/ResponseChecker";
+import MyService from "../service";
 
 const ReportApi = {
-    getChartData: async (body) => {
-        let result;
+  getChartData: async (body) => {
+    let result;
 
-        try {
-            result = await MyService.postRequestData(
-            "/owl/api/v1/get-chart", body
-            );
-        } catch (error) {
-            console.log(JSON.stringify(error));
-        }
-        if (handleErrCodeReport(result) === null) {
-            return [];
-        }
-        return result.payload;
-    },
+    try {
+      result = await MyService.postRequestData("/owl/api/v1/get-chart", body);
+    } catch (error) {
+      console.log(JSON.stringify(error));
+    }
+    if (handleErrCodeReport(result) === null) {
+      return [];
+    }
+    return result.payload;
+  },
 
-    getExportData: async (body) => {
-        console.log("body", body)
-        let result;
-        try {
-            result = await MyService.postRequestDataBlob(
-                "/owl/api/v1/export-excel", body
-                );
-            } catch (error) {
-                console.log(JSON.stringify(error));
-            }
-            
-            console.log("result", result)
-    
-        return result;
-    },
-}
+  getExportData: async (body) => {
+    let result;
+    try {
+      result = await MyService.postRequestDataBlob(
+        "/owl/api/v1/export-excel",
+        body
+      );
+    } catch (error) {
+      console.log(JSON.stringify(error));
+    }
 
+    return result;
+  },
+};
 
 export default ReportApi;
