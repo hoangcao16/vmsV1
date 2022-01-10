71f54696649516d1bda86abc7f3fcb7f6c3b243f
[BK]Phù Dung <thuongtt@edsolabs.com>
2022-01-10 10:34:04 +0700

    VR-1140 remove old code

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import { ConfigProvider } from 'antd';
-import moment from 'moment';
-import 'moment/locale/vi';
-import 'prismjs/themes/prism-tomorrow.css';
-import React from 'react';
-import { I18nextProvider } from 'react-i18next';
-import 'react-perfect-scrollbar/dist/css/styles.css';
-import i18n from './actions/i18n/i18n';
-import './components/@vuexy/rippleButton/RippleButton';
-import Router from './Router';
-import 'antd/dist/antd.css';
+import { ConfigProvider } from "antd";
+import moment from "moment";
+import "moment/locale/vi";
+import "prismjs/themes/prism-tomorrow.css";
+import React from "react";
+import { I18nextProvider } from "react-i18next";
+import "react-perfect-scrollbar/dist/css/styles.css";
+import i18n from "./actions/i18n/i18n";
+import "./components/@vuexy/rippleButton/RippleButton";
+import Router from "./Router";
+import "antd/dist/antd.css";
 
-moment.locale('vi-vn');
+moment.locale("vi-vn");
 
 const App = (props) => {
   return <Router />;
--- src/redux/reducers/rootReducer.js
+++ src/redux/reducers/rootReducer.js
@@ -3,15 +3,9 @@ import user from '../../view/user/dataListUser/redux/reducers';
 import cameraGroup from './../../../src/view/camera/redux/reducers/index';
 import notificationReducer from './../../view/notification/redux/reducers/index';
 import auth from './auth/';
-import calenderReducer from './calendar/';
 import cameraReducer from './camera/';
-import chatReducer from './chat/';
 import customizer from './customizer/';
-import dataList from './data-list/';
-import emailReducer from './email/';
 import mapReducer from './map';
-import navbar from './navbar/Index';
-import todoReducer from './todo/';
 import liveReducer from "./live"
 
 import chartData from './../../view/report/redux/reduders/index';
@@ -21,14 +15,8 @@ import quickSearch from './../../view/maps/redux/reducers/index';
 import openModalPresetSetting from './live/OpenModalPresetSettingReducers';
 
 const rootReducer = combineReducers({
-  calendar: calenderReducer,
-  emailApp: emailReducer,
-  todoApp: todoReducer,
-  chatApp: chatReducer,
   customizer: customizer,
   auth: auth,
-  navbar: navbar,
-  dataList: dataList,
   camera: cameraReducer,
   user: user,
   cameraGroup: cameraGroup,
