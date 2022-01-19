9782ae4f84ff82e89fb308f267925b1083653bd3
hainl <hainl@edsolabs.com>
2022-01-19 15:48:46 +0700

    VR-1171

--- src/redux/sagas/map/camera.js
+++ src/redux/sagas/map/camera.js
@@ -158,7 +158,7 @@ export function* addNewCamAction(action) {
       notifyMess.type = NOTYFY_TYPE.warning;
       if (language === "vn") {
         notifyMess.description =
-          error.response.data.errors.message || "Có sai ra từ phía máy chủ";
+          error.response.data.errors.message || "Có lỗi xảy ra từ phía máy chủ";
       } else {
         notifyMess.description =
           error.response.data.errors.message ||
