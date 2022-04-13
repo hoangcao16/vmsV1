67e085bed9f37c14f7e7c15122bd0b9627faac3c
Đặng Văn Tuấn <tuandv@edsolabs.com>
2022-04-13 06:57:54 +0000

    VR-1337

--- src/actions/api/tag/index.js
+++ src/actions/api/tag/index.js
@@ -1,81 +1,77 @@
-import {
-    responseCheckerErrorsController
-} from "../../function/MyUltil/ResponseChecker";
+import { responseCheckerErrorsController } from "../../function/MyUltil/ResponseChecker";
 import MyService from "../service";
-  const TagApi = {
-    getAllTags: async () => {
-      try {
-        const result = await MyService.getRequestData(
-          `/cctv-controller-svc/api/v1/tags_key`
-        );
-      if (responseCheckerErrorsController(result) === null) {
-        return [];
-      }
-      return result.payload;
-    } catch (error) {
-        console.log(error);
-      }
+const TagApi = {
+    getAllTags: async(data) => {
+        try {
+            const result = await MyService.getRequestData(
+                `/cctv-controller-svc/api/v1/tags_key?key=${data?.name}`
+            );
+            if (responseCheckerErrorsController(result) === null) {
+                return [];
+            }
+            return result.payload;
+        } catch (error) {
+            console.log(error);
+        }
     },
 
-  
-    getTagById: async (tagId) => {
-      try {
-      const result = await MyService.getRequestData(
-          `/cctv-controller-svc/api/v1/tags_key/${tagId}`
-        );
-      if (responseCheckerErrorsController(result) === null) {
-        return [];
-      }
-      return result.payload;
-    } catch (error) {
-        console.log(error);
-      }
+    getTagById: async(tagId) => {
+        try {
+            const result = await MyService.getRequestData(
+                `/cctv-controller-svc/api/v1/tags_key/${tagId}`
+            );
+            if (responseCheckerErrorsController(result) === null) {
+                return [];
+            }
+            return result.payload;
+        } catch (error) {
+            console.log(error);
+        }
     },
-  
-    updateTagById: async (tagId, payload) => {
-      try {
-        const result = await MyService.putRequestData(
-          `/cctv-controller-svc/api/v1/tags_key/${tagId}`,
-          payload
-        );
-      if (responseCheckerErrorsController(result) === null) {
-        return false;
-      }
-      return true;
-    } catch (error) {
-        console.log(error);
-      }
+
+    updateTagById: async(tagId, payload) => {
+        try {
+            const result = await MyService.putRequestData(
+                `/cctv-controller-svc/api/v1/tags_key/${tagId}`,
+                payload
+            );
+            if (responseCheckerErrorsController(result) === null) {
+                return false;
+            }
+            return true;
+        } catch (error) {
+            console.log(error);
+        }
     },
-  
-    deleteTagById: async (uuid) => {
-      let result;
-      try {
-        result = await MyService.deleteRequestData(
-          `/cctv-controller-svc/api/v1/tags_key/${uuid}`
-        );
-      if (responseCheckerErrorsController(result) === null) {
-        return false;
-      }
-      return true;
-    } catch (error) {
-        console.log(error);
-      }
+
+    deleteTagById: async(uuid) => {
+        let result;
+        try {
+            result = await MyService.deleteRequestData(
+                `/cctv-controller-svc/api/v1/tags_key/${uuid}`
+            );
+            if (responseCheckerErrorsController(result) === null) {
+                return false;
+            }
+            return true;
+        } catch (error) {
+            console.log(error);
+        }
     },
-    addTag: async (payload) => {
-      try {
-        const result = await MyService.postRequestData(
-          "/cctv-controller-svc/api/v1/tags_key",
-          payload
-        );
-      if (responseCheckerErrorsController(result) === null) {
-        return false;
-      }
-      return true;
-    } catch (error) {
-        console.log(error);
-      }
+    addTag: async(payload) => {
+        try {
+            const result = await MyService.postRequestData(
+                "/cctv-controller-svc/api/v1/tags_key",
+                payload
+            );
+            if (responseCheckerErrorsController(result) === null) {
+                return false;
+            }
+            return true;
+        } catch (error) {
+            console.log(error);
+        }
     },
-  };
-  
-  export default TagApi;
-  
+};
+
+export default TagApi;
