6b36d1de1c11ac0389ba575267e7c7dd44b58381
Nguyễn Hoàng <hoangnc@edsolabs.com>
2022-04-23 05:18:19 +0000

    [VR-1424][xử lý VPGT] Thiếu button chuyển

--- src/view/category/TableCategory.jsx
+++ src/view/category/TableCategory.jsx
@@ -4,7 +4,7 @@ import {
   InfoCircleOutlined,
   PlusOutlined,
   SearchOutlined,
-} from "@ant-design/icons";
+} from '@ant-design/icons';
 import {
   AutoComplete,
   Button,
@@ -14,62 +14,62 @@ import {
   Space,
   Table,
   Tooltip,
-} from "antd";
-import { isEmpty } from "lodash-es";
-import React, { useEffect, useState } from "react";
-import { useTranslation } from "react-i18next";
-import { withRouter } from "react-router-dom";
-import { reactLocalStorage } from "reactjs-localstorage";
-import CameraApi from "../../actions/api/camera/CameraApi";
-import DepartmentApi from "../../actions/api/department/DepartmentApi";
-import EventApi from "../../actions/api/event/EventApi";
-import FieldApi from "../../actions/api/field/FieldApi";
-import TagApi from "../../actions/api/tag";
-import VendorApi from "../../actions/api/vendor/VendorApi";
-import Notification from "../../components/vms/notification/Notification";
-import Breadcrumds from "../breadcrumds/Breadcrumds";
-import "./../commonStyle/commonInput.scss";
-import "./../commonStyle/commonSelect.scss";
-import "./../commonStyle/commonTable.scss";
-import ModalEditCategory from "./ModalEditCategory";
-import ModalUpdateTag from "./ModalUpdateTag";
-import ModalUpdateDepartment from "./ModalUpdateDepartment";
-import ModalViewDetail from "./ModalViewDetail";
-import "./TableCategory.scss";
-import { bodyStyleCard, headStyleCard } from "./variables";
-import debounce from "lodash/debounce";
+} from 'antd';
+import { isEmpty } from 'lodash-es';
+import React, { useEffect, useState, useCallback } from 'react';
+import { useTranslation } from 'react-i18next';
+import { withRouter } from 'react-router-dom';
+import { reactLocalStorage } from 'reactjs-localstorage';
+import CameraApi from '../../actions/api/camera/CameraApi';
+import DepartmentApi from '../../actions/api/department/DepartmentApi';
+import EventApi from '../../actions/api/event/EventApi';
+import FieldApi from '../../actions/api/field/FieldApi';
+import TagApi from '../../actions/api/tag';
+import VendorApi from '../../actions/api/vendor/VendorApi';
+import Notification from '../../components/vms/notification/Notification';
+import Breadcrumds from '../breadcrumds/Breadcrumds';
+import './../commonStyle/commonInput.scss';
+import './../commonStyle/commonSelect.scss';
+import './../commonStyle/commonTable.scss';
+import ModalEditCategory from './ModalEditCategory';
+import ModalUpdateTag from './ModalUpdateTag';
+import ModalUpdateDepartment from './ModalUpdateDepartment';
+import ModalViewDetail from './ModalViewDetail';
+import './TableCategory.scss';
+import { bodyStyleCard, headStyleCard } from './variables';
+import debounce from 'lodash/debounce';
 const AI_SOURCE = process.env.REACT_APP_AI_SOURCE;
 export const CATEGORY_NAME = {
-  EVENT_TYPE: "EVENT_TYPE",
-  VENDOR: "VENDOR",
-  CAMERA_TYPE: "CAMERA_TYPE",
-  FIELD: "FIELD",
-  TAGS: "TAGS",
-  DEPARTMENTS: "DEPARTMENTS",
+  EVENT_TYPE: 'EVENT_TYPE',
+  VENDOR: 'VENDOR',
+  CAMERA_TYPE: 'CAMERA_TYPE',
+  FIELD: 'FIELD',
+  TAGS: 'TAGS',
+  DEPARTMENTS: 'DEPARTMENTS',
 };
 
 const { Option } = Select;
 const TableCategory = () => {
   const { t } = useTranslation();
-  const language = reactLocalStorage.get("language");
+  const language = reactLocalStorage.get('language');
   const [dataOptions, setDataOptions] = useState({});
   const [dataType, setDataType] = useState(CATEGORY_NAME.VENDOR);
   const [selectedCategoryId, setSelectedCategoryId] = useState(null);
   const [selectedUnitId, setSelectedUnitId] = useState(null);
   const [showModal, setShowModal] = useState(false);
-  const [search, setSearch] = useState("");
+  const [search, setSearch] = useState('');
 
   useEffect(() => {
     if (
-      language === "vn"
-        ? (document.title = "CCTV | Quản lý danh mục")
-        : (document.title = "CCTV | Category Management")
+      language === 'vn'
+        ? (document.title = 'CCTV | Quản lý danh mục')
+        : (document.title = 'CCTV | Category Management')
     );
   }, [t]);
 
   useEffect(() => {
     const data = {
-      name: "",
+      name: '',
       size: 1000,
     };
     fetchOptionsData(data).then((data) => {
@@ -79,8 +79,8 @@ const TableCategory = () => {
 
   const handleChange = (value) => {
     setDataType(value);
-    setSearch("");
-    handleSearch("");
+    setSearch('');
+    handleSearch('');
   };
 
   const getDataByCategory = (dataType) => {
@@ -122,34 +122,34 @@ const TableCategory = () => {
     let name;
 
     if (dataType === CATEGORY_NAME.CAMERA_TYPE) {
-      name = `${t("view.map.camera_type", { cam: t("camera") })}`;
+      name = `${t('view.map.camera_type', { cam: t('camera') })}`;
     }
 
     if (dataType === CATEGORY_NAME.VENDOR) {
-      name = `${t("view.category.camera_vendor", { cam: t("camera") })}`;
+      name = `${t('view.category.camera_vendor', { cam: t('camera') })}`;
     }
 
     if (dataType === CATEGORY_NAME.FIELD) {
-      name = `${t("view.category.field")}`;
+      name = `${t('view.category.field')}`;
     }
 
     if (dataType === CATEGORY_NAME.EVENT_TYPE) {
-      name = `${t("view.category.event_type")}`;
+      name = `${t('view.category.event_type')}`;
     }
     if (dataType === CATEGORY_NAME.TAGS) {
-      name = `${t("view.category.tags")}`;
+      name = `${t('view.category.tags')}`;
     }
     if (dataType === CATEGORY_NAME.DEPARTMENTS) {
-      name = `${t("view.category.department")}`;
+      name = `${t('view.category.department')}`;
     }
 
     return (
-      <div className="card--header">
+      <div className='card--header'>
         <h4> {name}</h4>
-        <div className="search__toolbar">
+        <div className='search__toolbar'>
           <AutoComplete
-            className="searchInputCamproxy"
-            onSearch={debounce(handleSearch, 1000)}
+            className='searchInputCamproxy'
+            onSearch={handleSearch}
             onBlur={handleBlur}
             onPaste={handlePaste}
             maxLength={255}
@@ -158,38 +158,38 @@ const TableCategory = () => {
             style={{ width: 350, height: 40, marginRight: 18 }}
             placeholder={
               <div>
-                <span> &nbsp;{t("view.map.search")} </span>{" "}
+                <span> &nbsp;{t('view.map.search')} </span>{' '}
                 <SearchOutlined style={{ fontSize: 22 }} />
               </div>
             }
           ></AutoComplete>
           <Select defaultValue={CATEGORY_NAME.VENDOR} onChange={handleChange}>
             <Option value={CATEGORY_NAME.VENDOR}>
-              {t("view.category.camera_vendor", { cam: t("camera") })}
+              {t('view.category.camera_vendor', { cam: t('camera') })}
             </Option>
             <Option value={CATEGORY_NAME.FIELD}>
-              {t("view.category.field")}
+              {t('view.category.field')}
             </Option>
 
             <Option value={CATEGORY_NAME.CAMERA_TYPE}>
-              {t("view.map.camera_type", { cam: t("camera") })}
+              {t('view.map.camera_type', { cam: t('camera') })}
             </Option>
             <Option value={CATEGORY_NAME.EVENT_TYPE}>
-              {t("view.category.event_type")}
+              {t('view.category.event_type')}
             </Option>
             <Option value={CATEGORY_NAME.TAGS}>
-              {t("view.category.tags")}
+              {t('view.category.tags')}
             </Option>
-            {AI_SOURCE === "edso" && (
+            {AI_SOURCE === 'edso' && (
               <Option value={CATEGORY_NAME.DEPARTMENTS}>
-                {t("view.category.department")}
+                {t('view.category.department')}
               </Option>
             )}
           </Select>
 
-          <Tooltip placement="top" title={t("add")}>
+          <Tooltip placement='top' title={t('add')}>
             <Button
-              type="primary"
+              type='primary'
               onClick={() => {
                 setSelectedCategoryId(null);
                 setShowModal(true);
@@ -212,9 +212,15 @@ const TableCategory = () => {
     const value = event.target.value.trimStart();
     setSearch(value);
   };
-
-  const handleSearch = async (value) => {
-    setSearch(value);
+  const handleSearch = (query) => {
+    setSearch(query);
+    debounceSearch(query);
+  };
+  const debounceSearch = useCallback(
+    debounce((query) => handleSearchDB(query), 500),
+    []
+  );
+  const handleSearchDB = async (value) => {
     const data = {
       name: encodeURIComponent(value),
       size: 1000,
@@ -260,7 +266,7 @@ const TableCategory = () => {
     if (dataType === CATEGORY_NAME.DEPARTMENTS) {
       dataSearch = await DepartmentApi.getAllDepartment({
         ...data,
-        administrativeUnitUuid: "",
+        administrativeUnitUuid: '',
       });
       setDataOptions((prev) => {
         return { ...prev, departments: dataSearch };
@@ -279,11 +285,11 @@ const TableCategory = () => {
       isDelete = await CameraApi.deleteCameraType(id);
 
       const notifyMess = {
-        type: "success",
-        title: "",
-        description: `${t("noti.successfully_delete_camera_type", {
-          delete: t("delete"),
-          cam: t("camera"),
+        type: 'success',
+        title: '',
+        description: `${t('noti.successfully_delete_camera_type', {
+          delete: t('delete'),
+          cam: t('camera'),
         })}`,
       };
       isDelete && Notification(notifyMess);
@@ -292,11 +298,11 @@ const TableCategory = () => {
     if (dataType === CATEGORY_NAME.VENDOR) {
       isDelete = await VendorApi.delete(id);
       const notifyMess = {
-        type: "success",
-        title: "",
-        description: `${t("noti.successfully_delete_camera_vendor", {
-          delete: t("delete"),
-          cam: t("camera"),
+        type: 'success',
+        title: '',
+        description: `${t('noti.successfully_delete_camera_vendor', {
+          delete: t('delete'),
+          cam: t('camera'),
         })}`,
       };
       isDelete && Notification(notifyMess);
@@ -305,10 +311,10 @@ const TableCategory = () => {
     if (dataType === CATEGORY_NAME.FIELD) {
       isDelete = await FieldApi.deleteField(id);
       const notifyMess = {
-        type: "success",
-        title: "",
-        description: `${t("noti.successfully_delete_field", {
-          delete: t("delete"),
+        type: 'success',
+        title: '',
+        description: `${t('noti.successfully_delete_field', {
+          delete: t('delete'),
         })}`,
       };
       isDelete && Notification(notifyMess);
@@ -317,10 +323,10 @@ const TableCategory = () => {
     if (dataType === CATEGORY_NAME.EVENT_TYPE) {
       isDelete = await EventApi.deleteEvent(id);
       const notifyMess = {
-        type: "success",
-        title: "",
-        description: `${t("noti.successfully_delete_event_type", {
-          delete: t("delete"),
+        type: 'success',
+        title: '',
+        description: `${t('noti.successfully_delete_event_type', {
+          delete: t('delete'),
         })}`,
       };
       isDelete && Notification(notifyMess);
@@ -329,10 +335,10 @@ const TableCategory = () => {
     if (dataType === CATEGORY_NAME.TAGS) {
       isDelete = await TagApi.deleteTagById(id);
       const notifyMess = {
-        type: "success",
-        title: "",
-        description: `${t("noti.successfully_delete_tag_type", {
-          delete: t("delete"),
+        type: 'success',
+        title: '',
+        description: `${t('noti.successfully_delete_tag_type', {
+          delete: t('delete'),
         })}`,
       };
       isDelete && Notification(notifyMess);
@@ -341,16 +347,16 @@ const TableCategory = () => {
     if (dataType === CATEGORY_NAME.DEPARTMENTS) {
       isDelete = await DepartmentApi.delete(id);
       const notifyMess = {
-        type: "success",
-        title: "",
-        description: `${t("noti.successfully_delete_dep_type", {
-          delete: t("delete"),
+        type: 'success',
+        title: '',
+        description: `${t('noti.successfully_delete_dep_type', {
+          delete: t('delete'),
         })}`,
       };
       isDelete && Notification(notifyMess);
     }
     const data = {
-      name: "",
+      name: '',
       size: 1000,
     };
     fetchOptionsData(data).then(setDataOptions);
@@ -365,55 +371,55 @@ const TableCategory = () => {
 
   const categoryColumns = [
     {
-      title: `${t("view.storage.NO")}`,
-      key: "index",
-      className: "headerColums",
-      width: "10%",
+      title: `${t('view.storage.NO')}`,
+      key: 'index',
+      className: 'headerColums',
+      width: '10%',
       render: (text, record, index) => index + 1,
     },
 
     {
-      title: `${t("view.category.category_name")}`,
-      dataIndex: "name",
-      key: "name",
+      title: `${t('view.category.category_name')}`,
+      dataIndex: 'name',
+      key: 'name',
       ellipsis: true,
       // width: '50%',
-      className: "headerColums",
+      className: 'headerColums',
     },
 
     {
-      title: `${t("view.storage.action")}`,
-      className: "headerColums",
-      width: "12%",
+      title: `${t('view.storage.action')}`,
+      className: 'headerColums',
+      width: '12%',
       render: (_text, record) => {
         return (
           <Space>
-            <Tooltip placement="top" title={t("view.common_device.edit")}>
+            <Tooltip placement='top' title={t('view.common_device.edit')}>
               <EditOutlined
-                style={{ fontSize: "16px", color: "#6E6B7B" }}
+                style={{ fontSize: '16px', color: '#6E6B7B' }}
                 onClick={() => {
                   setSelectedCategoryId(record.uuid);
                   setShowModal(true);
                 }}
               />
             </Tooltip>
-            <Tooltip placement="top" title={t("delete")}>
+            <Tooltip placement='top' title={t('delete')}>
               <Popconfirm
-                title={t("noti.delete_category", { this: t("this") })}
-                cancelText={t("view.user.detail_list.cancel")}
-                okText={t("view.user.detail_list.confirm")}
+                title={t('noti.delete_category', { this: t('this') })}
+                cancelText={t('view.user.detail_list.cancel')}
+                okText={t('view.user.detail_list.confirm')}
                 onConfirm={() => handleDelete(record.uuid, dataType)}
               >
                 <DeleteOutlined
-                  style={{ fontSize: "16px", color: "#6E6B7B" }}
+                  style={{ fontSize: '16px', color: '#6E6B7B' }}
                 />
               </Popconfirm>
             </Tooltip>
 
             {dataType === CATEGORY_NAME.AD_DIVISIONS && (
-              <Tooltip placement="top" title={t("view.common_device.detail")}>
+              <Tooltip placement='top' title={t('view.common_device.detail')}>
                 <InfoCircleOutlined
-                  style={{ fontSize: "16px", color: "#6E6B7B" }}
+                  style={{ fontSize: '16px', color: '#6E6B7B' }}
                   onClick={() => {
                     setSelectedUnitId(record.uuid);
                   }}
@@ -427,29 +433,29 @@ const TableCategory = () => {
   ];
 
   const addFieldColumn = {
-    title: `${t("view.category.field")}`,
-    dataIndex: "fieldName",
-    key: "fieldName",
-    width: "40%",
+    title: `${t('view.category.field')}`,
+    dataIndex: 'fieldName',
+    key: 'fieldName',
+    width: '40%',
 
-    className: "headerColums",
+    className: 'headerColums',
   };
 
   const addTagColumns = [
     {
-      title: `${t("view.category.category_name")}`,
-      dataIndex: "key",
-      key: "key",
-      className: "headerColums",
+      title: `${t('view.category.category_name')}`,
+      dataIndex: 'key',
+      key: 'key',
+      className: 'headerColums',
     },
   ];
 
   const addDepartmentColumns = [
     {
-      title: `${t("view.category.administrative_unit")}`,
-      dataIndex: "administrativeName",
-      key: "administrativeName",
-      className: "headerColums",
+      title: `${t('view.category.administrative_unit')}`,
+      dataIndex: 'administrativeName',
+      key: 'administrativeName',
+      className: 'headerColums',
     },
   ];
 
@@ -496,27 +502,27 @@ const TableCategory = () => {
   };
 
   return (
-    <div className="tabs__container--category">
+    <div className='tabs__container--category'>
       <Breadcrumds
-        url="/app/setting"
-        nameParent={t("breadcrumd.setting")}
-        nameChild={t("view.user.category_management")}
+        url='/app/setting'
+        nameParent={t('breadcrumd.setting')}
+        nameChild={t('view.user.category_management')}
       />
 
       <Card
         title={getNameByCategory(dataType)}
         bodyStyle={bodyStyleCard}
         headStyle={headStyleCard}
-        className="card--category"
+        className='card--category'
       >
         <Table
           pagination={false}
           scroll={{ y: 500 }}
-          rowKey="id"
+          rowKey='id'
           columns={categoryColumns}
           dataSource={getDataByCategory(dataType)}
           locale={{
-            emptyText: `${t("view.user.detail_list.no_valid_results_found")}`,
+            emptyText: `${t('view.user.detail_list.no_valid_results_found')}`,
           }}
         />
       </Card>
@@ -538,7 +544,7 @@ async function fetchOptionsData(data) {
     FieldApi.getAllFeild(data),
     EventApi.getAllEvent(data),
     TagApi.getAllTags(data),
-    DepartmentApi.getAllDepartment({ ...data, administrativeUnitUuid: "" }),
+    DepartmentApi.getAllDepartment({ ...data, administrativeUnitUuid: '' }),
   ]);
 
   return {
